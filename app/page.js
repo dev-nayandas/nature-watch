8a655b028436dc36ca204a2520b032c5da6d0c5a
dev-nayandas <rich.nayan@gmail.com>
2025-01-22 01:49:44 +0600

    issue solved

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import { Suspense } from "react";
 
 export default function Home() {
   return (
-    <Suspense>
+    <Suspense fallback={<p>Loading...</p>}>
       <LocationDetector />
     </Suspense>
   );
